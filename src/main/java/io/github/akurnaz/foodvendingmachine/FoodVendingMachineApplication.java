7c234591961241940712728e282499f3cc43a736
Ali Kurnaz <alikurnaz_26@windowslive.com>
2020-09-30 15:07:21 +0300

    Initial commit

---
+++ src/main/java/io/github/akurnaz/foodvendingmachine/FoodVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package io.github.akurnaz.foodvendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodVendingMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodVendingMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/akurnaz/foodvendingmachine/FoodVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.akurnaz.foodvendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodVendingMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
